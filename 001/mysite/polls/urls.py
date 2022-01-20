ad01341f5c676f852f6c8d8fcdcece2fff63f535
ran <ran-n@tutanota.com>
2022-01-20 19:50:44 +0100

    Tutorial 03

--- 001/mysite/polls/urls.py
+++ 001/mysite/polls/urls.py
@@ -3,13 +3,21 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2022/01/16 15:03:04.624874
-#+ Editado:	2022/01/16 15:03:43.787467
+#+ Editado:	2022/01/20 19:49:15.902412
 # ------------------------------------------------------------------------------
 from django.urls import path
 from . import views
 # ------------------------------------------------------------------------------
+app_name = 'polls'
 urlpatterns = [
+        # /polls
         path('', views.index, name='index'),
+        # /pools/{id}
+        path('<int:question_id>/', views.detail, name='detail'),
+        # /pools/{id}/results
+        path('<int:question_id>/results', views.results, name='results'),
+        # /pools/{id}/vote
+        path('<int:question_id>/vote', views.vote, name='vote'),
         ]
 # ------------------------------------------------------------------------------
 
--- 001/mysite/polls/views.py
+++ 001/mysite/polls/views.py
@@ -3,12 +3,35 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2022/01/16 15:05:23.120443
-#+ Editado:	2022/01/16 15:05:29.056149
+#+ Editado:	2022/01/20 19:37:48.597401
 # ------------------------------------------------------------------------------
 
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Índice de enquisas')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+            'latest_question_list': latest_question_list,
+            }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    """
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = f'You\'re looking at the results of question {question_id}'
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f'You\'re voting on question {question_id}')
