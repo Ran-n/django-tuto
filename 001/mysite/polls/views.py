1e4036b76b391fd947a542d28021b63fd68399fd
ran <ran-n@tutanota.com>
2022-01-22 12:32:43 +0100

    1/2 Tuto4

--- 001/mysite/polls/views.py
+++ 001/mysite/polls/views.py
@@ -3,14 +3,15 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2022/01/16 15:05:23.120443
-#+ Editado:	2022/01/20 19:37:48.597401
+#+ Editado:	2022/01/22 12:32:07.962247
 # ------------------------------------------------------------------------------
-
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.db.models import F
 
-from .models import Question
-
+from .models import Question, Choice
+# ------------------------------------------------------------------------------
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -19,19 +20,36 @@ def index(request):
             }
     return render(request, 'polls/index.html', context)
 
+
 def detail(request, question_id):
-    """
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist')
-    """
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    return render(request, 'polls/detail.html', {
+        'question': question,
+        })
+
 
 def results(request, question_id):
-    response = f'You\'re looking at the results of question {question_id}'
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {
+            'question': question
+        })
+
 
 def vote(request, question_id):
-    return HttpResponse(f'You\'re voting on question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # volve ensinar o form de votar
+        return render(request, 'polls/detail.html', {
+                'question': question,
+                'error_message': 'You didn\'t select a choice.',
+            })
+    else:
+        #selected_choice.votes += 1
+        # para evitar race condition
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # sempre devolver algo tras un POST para que o usuario non lle de varias veces
+        #ó votón de enviar.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
